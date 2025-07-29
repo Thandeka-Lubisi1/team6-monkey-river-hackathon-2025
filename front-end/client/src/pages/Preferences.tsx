da2e28beb7f46787ac1dbd9155d161a30e2c561f
hackerswrld <ebuangkamogelo@gmail.com>
2025-07-29 17:08:51 +0200

    preference page, change username, email and password forms

--- front-end/client/src/pages/Preferences.tsx
+++ front-end/client/src/pages/Preferences.tsx
@@ -1,15 +1,61 @@
 import { Link } from 'react-router-dom'
+// import { Sidebar, SidebarContent, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+// import ChangePassword from '@/components/ui/preferenceforms/changeUserN';
+import ChangeUserName from '@/components/ui/preferenceforms/changeUserN';
+import ChangePassword from '@/components/ui/preferenceforms/changePassword';
+import ChangeEmail from '@/components/ui/preferenceforms/changeEmail';
+
 
 const Preferences = () => {
   return (
     <div>
       <h1>Preference Page</h1>
-      <p>Welcome to the preference page!</p>
-      <nav>
-        <Link to="/about">Go to About</Link>
-        <br />
-        <Link to="/contact">Go to Contact</Link>
-      </nav>
+      <Accordion
+      type="single"
+      collapsible
+      className="w-full"
+      defaultValue="item-1"
+    >
+      <AccordionItem value="item-1">
+        <AccordionTrigger>User Name</AccordionTrigger>
+        <AccordionContent className="flex flex-col gap-4 text-balance">
+          <p>
+           Change user name
+          </p>
+          <div>
+            <ChangeUserName/>
+          </div>
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-2">
+        <AccordionTrigger>change email</AccordionTrigger>
+        <AccordionContent className="flex flex-col gap-4 text-balance">
+          <p>
+            Change email
+          </p>
+          <div>
+            <ChangeEmail/>
+          </div>
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-3">
+        <AccordionTrigger>Change Password</AccordionTrigger>
+        <AccordionContent className="flex flex-col gap-4 text-balance">
+          <p>
+            Change Password
+          </p>
+          <div>
+            <ChangePassword/>
+          </div>
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
     </div>
   )
 }
