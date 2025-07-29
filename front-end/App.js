40713381fef280c2ec9f6b1ca8c77c28aef0d7fb
gundort <gtshavhungwe@gmail.com>
2025-07-29 15:00:31 +0200

    Authuser
    Here's everything for the login and registration
    
    Signed-off-by: gundort <gtshavhungwe@gmail.com>

---
+++ front-end/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AuthToggle from './AuthToggle';
+
+function App() {
+  return (
+    <div style={{ background: '#e5e7eb', height: '100vh', paddingTop: '50px' }}>
+      <AuthToggle />
+    </div>
+  );
+}
+
+export default App;
